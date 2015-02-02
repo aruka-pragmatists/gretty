4bfefcd11511b1c6a24e9c90f4a94d3f6330e8e3
Adam Ruka <adam.ruka@pragmatists.pl>
2015-02-02 14:17:32 +0100

    Turn off the Spring Boot Launcher.

--- libs/gretty/src/main/groovy/org/akhikhl/gretty/ProjectUtils.groovy
+++ libs/gretty/src/main/groovy/org/akhikhl/gretty/ProjectUtils.groovy
@@ -199,7 +199,11 @@ final class ProjectUtils {
   // ATTENTION: this function resolves compile configuration!
   static boolean isSpringBootApp(Project project) {
     def compileConfig = project.configurations.findByName('compile')
-    compileConfig && compileConfig.resolvedConfiguration.resolvedArtifacts.find { it.moduleVersion.id.group == 'org.springframework.boot' }
+    def ret = compileConfig && compileConfig.resolvedConfiguration.resolvedArtifacts.find {
+        it.moduleVersion.id.group == 'org.springframework.boot'
+    }
+    log.trace(ret.toString())
+    false
   }
 
   static boolean isSpringBootApp(Project project, WebAppConfig wconfig) {
